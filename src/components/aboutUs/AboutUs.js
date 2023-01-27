7a138ecca36f221c56e035668aaac9271e658247
SAAD <msaad11844@gmail.com>
2023-01-28 01:12:49 +0500

    testing

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -4,7 +4,7 @@ import styles from './aboutUs.module.css';
 export default function AboutUs() {
     return (
         <div  >
-              <h2 >Who We Are</h2>
+              <h2 >Who We Are Testing</h2>
             <div className={styles.maincontainer}>
             <span >
                     Welkin Solutions (Pvt.) Ltd. which is involved in HVAC and Clean Room solutions business for the last many years. We have a team of skilled, experienced and specialized HVAC engineers and clean room experts who can manage all the espects of project, from design to execution. We as HVAC company is providing turnkey solution i.e. supply of HVAC equipment, installation of HVAC equipment, testing, commissioning and after sales services.

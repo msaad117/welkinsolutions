cd79de2cdebffba90e8cc49f964dd1b1f1bc6797
SAAD <msaad11844@gmail.com>
2023-02-06 23:26:38 +0500

    changes

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -3,21 +3,19 @@ import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import { Zoom } from "react-slideshow-image";
 import styles from './Slider.module.css';
-import Profile1 from "../../assets/image1.jpeg"
-import Profile2 from "../../assets/image2.jpeg"
+import Profile1 from "../../assets/1200x500 AHUS.jpg"
+import Profile2 from "../../assets/1200x500.jpg"
 import Profile3 from "../../assets/Clients/Industrial Ventilation Fan.jpg"
-import Profile4 from "../../assets/image3.jpeg"
-import Profile5 from "../../assets/image4.jpeg"
-import Profile6 from "../../assets/image5.jpeg"
-import Profile7 from "../../assets/image6.jpeg"
+import Profile4 from "../../assets/1200x500 fan coil units.jpg"
+import Profile5 from "../../assets/1200x500 clean rooms.jpg"
 
 const fadeImages = [
-    Profile1,Profile2,Profile3,Profile4,Profile5,Profile6,Profile7
+    Profile1,Profile2,Profile3,Profile4,Profile5
 ];
 const zoomOutProperties = {
     duration: 4000,
     transitionDuration: 500,
-    infinite: false,
+    infinite: true,
     indicators: true,
     scale: 0.4,
     arrows: true

5346a63742df0380cf8ead037a2742cacb2db02a
SAAD <msaad11844@gmail.com>
2023-02-10 17:49:37 +0500

    changes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,11 +1,22 @@
 import * as React from "react";
-
+import contact from "../../assets/bg-contact.jpg"
+import styles from './Contact.module.css';
+import map from "../../assets/map (1).png"
 
 function Contact() {
 return(
-    
-    <div style={{"textAlign":"center"}}>
-    <img src="https://etu.nz/wp-content/uploads/2019/06/under-maintenance-sign-500x500.jpg"/>
+    <div>
+    <div style={{ backgroundImage: `url(${contact})`,height:"420px",marginTop:"0px" ,textAlign:"center"}}>
+    <div className={styles.textContainer}>
+    <h1 className={styles.headingText}>Contact Welkin Solutions </h1>
+    <div className={styles.innerText}>
+    We are here to serve you from 9:00 am to 6:00 pm in the office and 24/7 online
+    </div>
+    </div>
+    </div>
+    <div style={{textAlign:"center",marginBottom:"25px",marginTop:"25px"}}>
+   <img className={styles.imageCard} src={map}></img>
+      </div>
     </div>
     
 )
